bc115fb8b469dd8ea188d02d7833100daa28b8be
Abhishek Kumar Gupta <its.diamond8990@gmail.com>
2026-06-06 21:11:21 +0530

    Day6 uploaded

---
+++ Day6/power_n.java
@@ -0,0 +1,21 @@
+package Day6;
+import java.util.Scanner;
+public class power_n {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int n = scan.nextInt();
+
+        System.out.print("Enter the power: ");
+        int p = scan.nextInt();
+
+        int result = 1;
+        while(p>0){
+            result*=n;
+            p--;
+        }
+        System.out.println("The result is "+result);
+        scan.close();
+    }
+}
