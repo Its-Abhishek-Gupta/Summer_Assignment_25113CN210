74a88a3d21ba07cb79ec04e612f2e1d419185b1c
Abhishek Kumar Gupta <its.diamond8990@gmail.com>
2026-06-08 21:35:30 +0530

    Day 8 uploaded

---
+++ Day8/Half_pyramid.java
@@ -0,0 +1,18 @@
+package Day8;
+import java.util.Scanner;
+public class Half_pyramid {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows in the pyramid: ");
+        int r = scan.nextInt();
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        scan.close();
+    }
+}
