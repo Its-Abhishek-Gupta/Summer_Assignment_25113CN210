8bb432a03d4aba4cdf254f512d2865b86c1700b6
Abhishek Kumar Gupta <its.diamond8990@gmail.com>
2026-06-29 12:09:16 +0530

    Assignment uploaded

---
+++ Day07/Recursive_factorial.java
@@ -0,0 +1,20 @@
+package Day07;
+import java.util.Scanner;
+public class Recursive_factorial {
+
+    static long factorial(int n){
+        if (n==0 || n==1)
+            return 1;
+        return n*factorial(n-1);
+    }
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the number to find the factorial: ");
+        int n = scan.nextInt();
+
+        System.out.println("The factorial of "+n+" is "+factorial(n));
+
+        scan.close();
+    }
+}
