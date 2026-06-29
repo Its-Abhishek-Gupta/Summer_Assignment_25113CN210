a8c5ae27209a7eada626585afa487374b2b7dbcf
Abhishek Kumar Gupta <its.diamond8990@gmail.com>
2026-06-29 12:18:07 +0530

    Assignment uploaded

---
+++ Day09/RepeatedCharacter_pattern.java
@@ -0,0 +1,11 @@
+package Day09;
+public class RepeatedCharacter_pattern {
+    public static void main(String[] args){
+        for (int i=1;i<=5;i++){
+            for (int j=1;j<=i;j++){
+                System.out.print((char)(64+i)+" ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
